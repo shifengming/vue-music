7031d98dd6f88e3df80b8f3494d7966fd2795ec9
sfm <2231962562@qq.com>
2019-10-30 17:41:16 +0800

    这是推荐页面轮播图效果

---
+++ src/api/recommend.js
@@ -0,0 +1,28 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getRecommend(){
+    const url= 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+
+    const data = Object.assign({},commonParams,{
+        platform: 'h5',
+        uin: 0,
+        needNewCode: 1
+    })
+    return jsonp(url, data, options)
+}
+export function getDiscList(){
+    const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+
+    const data = Object.assign({},commonParams,{
+        platform: 'yqq',
+        hostUin: 0,
+        sin: 0,
+        ein: 29,
+        sortId: 5,
+        needNewCode: 0,
+        categoryId: 10000000,
+        rnd: Math.random()
+    })
+    return jsonp(url, data, options)
+}
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="recommend">
+        <div class="recommend-content">
+            <div v-if="recommends.length" class="slider-wrapper">
+                <slider>
+                    <div v-for="item in recommends">
+                        <a :href="item.linkUrl">
+                            <img :src="item.picUrl"/>
+                        </a>
+                    </div>
+                </slider>
+             </div>
+            <div class="recommend-list">
+                <h1 class="list-title">热门歌单推荐</h1>
+                <ul>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+<script type="text/ecmascipt-6">
+    import Slider from 'base/slider/slider'
+    import {getRecommend, getDiscList} from 'api/recommend'
+    import {ERR_OK} from 'api/config'
+
+    export default {
+        data() {
+            return {
+                recommends : [],
+                discList: []
+            }
+        },
+        created() {
+            this._getRecommend()
+            this._getDiscList()
+        },
+        methods: {
+            _getRecommend(){
+                getRecommend().then((res) => {
+                    if(res.code === ERR_OK){
+                      this.recommends = res.data.slider
+                    }
+                })
+            },
+            _getDiscList(){
+              getDiscList().then((res) => {
+                if (res.code === ERR_OK){
+                  // console.log(res.data.list)
+                  this.discList = res.data.list
+                }
+              })
+            }
+        },
+        components:{
+            Slider
+        }
+    }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    @import "~common/stylus/variable"
+
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
+
+</style>
