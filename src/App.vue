5cc36d0349bdd74630516f243e7da320293ee7d9
tangmengcheng <1018663766@qq.com>
2019-11-14 00:55:18 +0800

    delete zhushi

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,6 @@
 <script>
 import MHeader from 'components/m-header/m-header'
 import Tab from 'components/tab/tab'
-// import Player from 'components/player/player'
 export default {
   name: 'App',
   components: {
