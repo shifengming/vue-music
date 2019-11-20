e58099ad102fd1e65a2fec6b4c03b058480ca7f1
tangmengcheng <1018663766@qq.com>
2019-11-20 14:53:10 +0800

    修改报错

--- src/base/progress-bar/progress-bar.vue
+++ src/base/progress-bar/progress-bar.vue
@@ -1,87 +1,93 @@
 <template>
     <div class="progress-bar" ref="progressBar" @click="progressClick">
-        <div class="bar-inner">
-            <!-- 整个进度条 -->
-            <div class="progress" ref="progress"></div>
-            <div class="progress-btn-wrapper" ref="progressBtn"
-                @touchstart.prevent="progressTouchStart"
-                @touchmove.prevent="progressTouchMove"
-                @touchend.prevent="progressTouchEnd"
-            >
-                <div class="progress-btn"></div>
-            </div>
+      <div class="bar-inner">
+        <!--整个进度条-->
+        <div class="progress" ref="progress"></div>
+        <!--注意：在操作进度条上小球的移动时候要禁止浏览器的默认行为prevent-->
+        <div class="progress-btn-wrapper" ref="progressBtn"
+             @touchstart.prevent="progressTouchStart"
+             @touchmove.prevent="progressTouchMove"
+             @touchend.prevent="progressTouchEnd"
+        >
+          <!--进度条当前的位置-->
+          <div class="progress-btn"></div>
         </div>
+      </div>
     </div>
 </template>
 
-<script type="text/ecmascript-6">
-    import {prefixStyle} from 'common/js/dom'
-    const progressBtnWidth = 16
-    const transform = prefixStyle('transform')
-
-    export default{
-        props:{
-            percent:{
-                type: Number,
-                default: 0
-            }
-        },
-        created(){
-            this.touch = {}
-        },
-        methods:{
-            progressTouchStart(e){
-                this.touch.initiated = true
-                this.touch.startX = e.touches[0].pageX
-                this.touch.left = this.$refs.progress.clientWidth
-            },
-            progressTouchMove(e){
-                if(!this.touch.initiated){
-                    return
-                }
-                const deltaX = e.touches[0].pageX - this.touch.startX
-                const offsetWidth = Math.min(this.$refs.progressBar.clientWidth - progressBtnWidth,Math.max(0,this.touch.left + deltaX))
-                this._offset(offsetWidth)
-            },
-            progressTouchEnd(){
-                this.touch.initiated = false
-                this._triggerPercent()
-            },
-            progressClick(e){
-                const rect = this.$refs.progressBar.getBoundingClientRect()
-                const offsetWidth = e.pageX - rect.left
-                this._offset(offsetWidth)
-                //这里当我们点击progressBtn的时候，e.offsetX获取不对
-                // this._offset(e.offsetX)
-                this._triggerPercent()
-            },
-            _triggerPercent(){
-                 const barWidth = this.$refs.progressBar.clientWidth - progressBtnWidth
-                 const percent = this.$refs.progress.clientWidth / barWidth
-                 this.$emit('percentChange',percent)
-            },
-            _offset(offsetWidth){
-                    this.$refs.progress.style.width = `${offsetWidth}px`
-                    this.$refs.progressBtn.style[transform] = `translate3d(${offsetWidth}px,0,0)`
-            }
-
-        },
-        //通过watch percent的变化
-        watch:{
-            percent(newPercent){
-                if(newPercent >= 0 && !this.touch.initiated){
-                    const barWidth = this.$refs.progressBar.clientWidth - progressBtnWidth
-                    const offsetWidth = newPercent * barWidth
-                    this._offset(offsetWidth)
-                }
-            }
-        }
+<script>
+import {prefixStyle} from 'common/js/dom'
+// 进度条上小球的宽度
+const progressBtnWidth = 16
+const transform = prefixStyle('transform')
+export default {
+  name: 'progress-bar',
+  props: {
+    percent: { // 进度条的百分比
+      type: Number,
+      default: 0
     }
+  },
+  created () {
+    // 移动小球时下面三个时间可以将内容挂载到touch对象上
+    this.touch = {}
+  },
+  methods: {
+    progressTouchStart (e) {
+      this.touch.initiated = true // 表示小球已经初始化了
+      this.touch.startX = e.touches[0].pageX // 记录手指点击小球后第一个横向坐标
+      this.touch.left = this.$refs.progress.clientWidth // 记录小球已经在进度条上滚动距离坐标的长度
+    },
+    progressTouchMove (e) {
+      if (!this.touch.initiated) {
+        return
+      }
+      const deltaX = e.touches[0].pageX - this.touch.startX // 记录小球被拉了多长
+      const offsetWidth = Math.min(this.$refs.progressBar.clientWidth - progressBtnWidth, Math.max(0, this.touch.left + deltaX))
+      this._offset(offsetWidth)
+    },
+    progressTouchEnd () {
+      this.touch.initiated = false // 移动小球结束后将初始化数据变为false
+      this._triggerPercent()
+    },
+    _offset (offsetWidth) {
+      this.$refs.progress.style.width = `${offsetWidth}px`
+      this.$refs.progressBtn.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+    },
+    _triggerPercent () {
+      const barWidth = this.$refs.progressBar.clientWidth - progressBtnWidth
+      const percent = this.$refs.progress.clientWidth / barWidth
+      this.$emit('percentChange', percent)
+    },
+    progressClick (e) {
+      const rect = this.$refs.progressBar.getBoundingClientRect()
+      const offsetWidth = e.pageX - rect.left // e.pageX是获取小球到浏览器边缘的距离，getBoundingClientRect是进度条最左边到浏览器边缘的距离
+      this._offset(offsetWidth)
+      // 这里当我们点击progressBtn的时候，e.offsetX获取不对
+      // this._offset(e.offsetX)
+      this._triggerPercent()
+    }
+  },
+  watch: {
+    // 监测外面传来的百分比
+    percent (newPercent) {
+      if (newPercent >= 0 && !this.touch.initiated) {
+        // 整个进度条的宽度
+        const barWidth = this.$refs.progressBar.clientWidth - progressBtnWidth
+        // 小球在进度条上向左偏移的长度
+        const offsetWidth = newPercent * barWidth
+        // this.$refs.progress.style.width = `${offsetWidth}px`
+        // this.$refs.progressBtn.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+        this._offset(offsetWidth)
+      }
+    }
+  }
+}
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
-    @import "~common/stylus/variable"
-
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+@import "~common/stylus/variable"
 .progress-bar
   height: 30px
   .bar-inner
--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -1,98 +1,99 @@
 <template>
     <div ref="wrapper">
-        <slot></slot>
+      <slot></slot>
     </div>
 </template>
-<script type="text/ecmascript-6">
-    import BScroll from 'better-scroll'
 
-    export default{
-        name: 'scroll',
-        props:{
-            probeType: {
-                type: Number,
-                default: 1
-            },
-            click:{
-                type: Boolean,
-                default: true
-            },
-            data:{
-                type: Array,
-                default: null
-            },
-            listenScroll:{//要不要监听事件
-                type: Boolean,
-                default: false
-            },
-            pullup: {
-                type: Boolean,
-                default: false
-                },
-            // 解决移动端失去焦点输入框隐藏
-            beforeScroll: {
-                type: Boolean,
-                default: false
-            }
-        },
-        mounted(){
-            setTimeout(() => {
-                this._initScroll()
-            },20)
-        },
-        methods:{
-            _initScroll(){
-                if(!this.$refs.wrapper){
-                    return
-                }
-                this.scroll = new BScroll(this.$refs.wrapper,{
-                    probeType: this.probeType,
-                    click: this.click
-                })
-                if(this.listenScroll){
-                    let me = this
-                    this.scroll.on('scroll',(pos) => {
-                        this.$emit('scroll',pos)
-                    })
-                }
-                 if (this.pullup) {
-                    this.scroll.on('scrollEnd', () => {
-                    if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
-                        this.$emit('scrollToEnd')
-                    }
-                    })
-                }
-                if (this.beforeScroll) {
-                    this.scroll.on('beforeScrollStart', () => {
-                    this.$emit('beforeScroll')
-                    })
-                }
-                },
-            enable(){
-                this.scroll && this.scroll.enable()
-            },
-            disable(){
-                this.scroll && this.scroll.disable()
-            },
-            refresh(){
-                this.scroll && this.scroll.refresh()
-            },
-            scrollTo(){
-                this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
-            },
-            scrollToElement(){
-                this.scroll && this.scrollToElement.apply(this.scroll, arguments)
-            }
-        },
-        watch:{
-            data(){
-                setTimeout(() => {
-                    this.refresh()
-                },20)
-            }
-        }
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'scroll',
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    data: {
+      type: Array,
+      default: null
+    },
+    listenScroll: { // 要不要监听事件
+      type: Boolean,
+      default: false
+    },
+    pullup: {
+      type: Boolean,
+      default: false
+    },
+    // 解决移动端失去焦点输入框隐藏
+    beforeScroll: {
+      type: Boolean,
+      default: false
     }
+  },
+  mounted () {
+    setTimeout(() => {
+      this._initScroll()
+    }, 20)
+  },
+  methods: {
+    _initScroll () {
+      if (!this.$refs.wrapper) {
+        return
+      }
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        probeType: this.probeType,
+        click: this.click
+      })
+      if (this.listenScroll) {
+        let me = this
+        this.scroll.on('scroll', (pos) => {
+          me.$emit('scroll', pos)
+        })
+      }
+      if (this.pullup) {
+        this.scroll.on('scrollEnd', () => {
+          if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+            this.$emit('scrollToEnd')
+          }
+        })
+      }
+      if (this.beforeScroll) {
+        this.scroll.on('beforeScrollStart', () => {
+          this.$emit('beforeScroll')
+        })
+      }
+    },
+    enabled () {
+      this.scroll && this.scroll.enable()
+    },
+    disable () {
+      this.scroll && this.scroll.disable()
+    },
+    refresh () {
+      this.scroll && this.scroll.refresh()
+    },
+    scrollTo () {
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+    },
+    scrollToElement () {
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+    }
+  },
+  watch: {
+    data () {
+      setTimeout(() => {
+        this.refresh()
+      }, 20)
+    }
+  }
+}
 </script>
-<style scoped lang="stylus" rel="stylesheet/stylus">
+
+<style scoped>
 
 </style>
